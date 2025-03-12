ea3cd01dd6387291d40af6796e89ec51b15ebb46
TaLePhu <lephu18062@gmail.com>
2025-03-12 19:45:49 +0700

    Done home page, banner, carousel

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Navbar from './layouts/Navbar';
 import Footer from './layouts/Footer';
+import HomePage from './layouts/homepage/HomePage';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <HomePage/>
       <Footer/>
     </div>
   );
---
+++ src/layouts/homepage/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "./components/Banner";
+import Carousel from "./components/Carousel";
+
+const HomePage = () => {
+    return(
+        <div>
+            <Banner/>
+            <Carousel/>
+        </div>
+    )
+}
+
+export default HomePage;
