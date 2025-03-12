97e81affab309f462121d6e0674dda0855a2ee05
TaLePhu <lephu18062@gmail.com>
2025-03-12 16:58:08 +0700

    done footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './layouts/Navbar';
+import Footer from './layouts/Footer';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Footer/>
     </div>
   );
 }
