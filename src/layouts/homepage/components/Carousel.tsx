e4855fbc1ca7642defe1dea9700559561566a2db
TaLePhu <lephu18062@gmail.com>
2025-04-08 20:38:16 +0700

    config prettier

--- src/layouts/homepage/components/Carousel.tsx
+++ src/layouts/homepage/components/Carousel.tsx
@@ -1,28 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { lay3SachMoiNhat } from "../../../api/SachAPI";
-import SachModel from "../../../models/SachModel";
-import CarouselItem from "./CarouselItem";
+import React, { useEffect, useState } from 'react';
+import { lay3SachMoiNhat } from '../../../api/SachAPI';
+import SachModel from '../../../models/SachModel';
+import CarouselItem from './CarouselItem';
 
 const Carousel: React.FC = () => {
-
     const [danhSachQuyenSach, setDanhSachQuyenSach] = useState<SachModel[]>([]);
     const [dangTaiDuLieu, setDangTaiDuLieu] = useState(true);
     const [baoLoi, setBaoLoi] = useState(null);
 
-    useEffect(() => {
-        lay3SachMoiNhat().then(
-            sachData => {
-                setDanhSachQuyenSach(sachData);
-                setDangTaiDuLieu(false);
-            }
-        ).catch(
-            error => {
-                setDangTaiDuLieu(false);
-                setBaoLoi(error.message);
-            }
-        );
-    }, [] // Chi goi mot lan
-    )
+    useEffect(
+        () => {
+            lay3SachMoiNhat()
+                .then((sachData) => {
+                    setDanhSachQuyenSach(sachData);
+                    setDangTaiDuLieu(false);
+                })
+                .catch((error) => {
+                    setDangTaiDuLieu(false);
+                    setBaoLoi(error.message);
+                });
+        },
+        [], // Chi goi mot lan
+    );
 
     if (dangTaiDuLieu) {
         return (
@@ -40,31 +39,41 @@ const Carousel: React.FC = () => {
         );
     }
 
-    return(
+    return (
         <div>
             <div id="carouselExampleDark" className="carousel carousel-dark slide">
                 <div className="carousel-inner">
                     <div className="carousel-item active" data-bs-interval="10000">
-                        <CarouselItem key={0} sach={danhSachQuyenSach[0]}/>
+                        <CarouselItem key={0} sach={danhSachQuyenSach[0]} />
                     </div>
                     <div className="carousel-item " data-bs-interval="10000">
-                    <CarouselItem key={1} sach={danhSachQuyenSach[1]}/>
+                        <CarouselItem key={1} sach={danhSachQuyenSach[1]} />
                     </div>
                     <div className="carousel-item " data-bs-interval="10000">
-                    <CarouselItem key={2} sach={danhSachQuyenSach[2]}/>
+                        <CarouselItem key={2} sach={danhSachQuyenSach[2]} />
                     </div>
                 </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                <button
+                    className="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselExampleDark"
+                    data-bs-slide="prev"
+                >
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span className="visually-hidden">Previous</span>
                 </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                <button
+                    className="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselExampleDark"
+                    data-bs-slide="next"
+                >
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                     <span className="visually-hidden">Next</span>
                 </button>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Carousel;
